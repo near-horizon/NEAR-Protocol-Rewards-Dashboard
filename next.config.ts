4ee642343354d333c0184ab37af9265abd171b97
lucianom <martinslucianofigueira@gmail.com>
2025-04-09 14:42:48 -0300

    fix url cors error

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: '/api/dashboard',
-        destination: 'https://near-protocol-rewards-tracking.com/dashboard',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
